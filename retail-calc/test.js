34384209d41e31b54d49cd11beab59c84c6a9243
Torok Gabor <gabor@20y.hu>
2020-01-07 10:26:20 +0100

    Add few more example test cases

--- retail-calc/test.js
+++ retail-calc/test.js
@@ -4,11 +4,20 @@ const assert = chai.assert;
 
 describe('Calculates total price with discount', function() {
 
-    // Add your test cases here
     // Use Chai Assertion Library: https://www.chaijs.com/api/assert/
 
     it('Throws error if no argument was given', function() {
         assert.throws(() => calculator(), Error);
     });
+
+    it('Takes quantity and unit price and calculates total', function() {
+        assert.equal(246, calculator(12, 20.5));
+    });
+
+    it('Gets 3% discount over $1000 total', function() {
+        assert.equal(989.4, calculator(100, 10.2));
+    });
+
+    // Add more test cases here
 
 });
