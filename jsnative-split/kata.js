8f2e120897e56e625a8f4671f2705da4c582f48a
Torok Gabor <gabor@20y.hu>
2020-02-06 13:24:10 +0100

    Add solution and further test cases for JavaScript split kata

--- jsnative-split/kata.js
+++ jsnative-split/kata.js
@@ -1,5 +1,21 @@
 const split = function(str, separator) {
-    // Add your solution here
+    let words = [];
+    let word = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == separator) {
+            words.push(word);
+            word = "";
+        } else {
+            word += str[i];
+        }
+    }
+    words.push(word);
+    return words;
 };
+
+String.prototype.mySplit = function(separator) {
+    return split(this, separator);
+};
+
 
 module.exports = split;
--- jsnative-split/test.js
+++ jsnative-split/test.js
@@ -13,12 +13,25 @@ describe('Splits string into elements and returns array', function() {
         assert.deepEqual(split("hello world", " "), ["hello", "world"]);
     });
 
+    it('Returns the right number of elements', function() {
+        let res = split("hello world", " ");
+        assert.equal(res.length, 2);
+    });
+
+    it('If no separator is given, return string as array', function() {
+        assert.deepEqual(split("hello world"), ["hello world"]);
+    });
+
 });
 
 describe('Can be invoked on String object', function() {
 
-    it('mySplit is a function', function() {
+    it('mySplit() is a function', function() {
         assert.equal(typeof "hello world".mySplit, "function");
+    });
+
+    it('mySplit() works as split()', function() {
+        assert.deepEqual("hello world".mySplit(), ["hello world"]);
     });
 
 });
